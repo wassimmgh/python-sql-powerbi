c8e85d3d8929f8e1e2516e394556de39fb1bd9c4
wassim_mgh <167897728+wassimmgh@users.noreply.github.com>
2026-03-14 11:45:22 +0000

    Add queries for order period and null customer count

--- sql/01_exploration.sql
+++ sql/01_exploration.sql
@@ -3,3 +3,12 @@
 # TOTAL ROWS NUMBER IN THE TABLE :
 SELECT COUNT(*) AS total_rows
 FROM superstore;
+
+# SELLS PERIOD
+SELECT MIN (`Order Date`) AS first_order,
+       MAX (`Order Date`) AS last_order
+FROM superstore;
+
+# Check NULL VALUES
+SELECT COUNT (*) - COUNT(`Customer ID`) AS null_customers
+FROM superstore;
