09c352f55420641c93393a7d3aee0e2274448322
wassim_mgh <167897728+wassimmgh@users.noreply.github.com>
2026-03-14 11:56:20 +0000

    Update 02_kpi.sql

--- sql/02_kpi.sql
+++ sql/02_kpi.sql
@@ -1,1 +1,21 @@
+# 2_ KPIs BUSINESS :
 
+# TOTAL SALES :
+SELECT ROUND (SUM(Sales),2) AS Total_sales
+FROM superstore;
+
+# TOTAL PROFIT :
+SELECT ROUND(SUM(Profit),2) AS Total_profit
+FROM superstore;
+
+# MARGIN % :
+SELECT ROUND (SUM(Profit)/SUM(Sales)*100,2) AS Margin_pct
+From superstore;
+
+# TOTAL ORDERS :
+SELECT COUNT (DISTINCT `Order ID`) AS Total_orders
+From superstore;
+
+# TOTAL CUSTOMERS :
+SELECT COUNT(DISTINCT `Customer ID`) AS Total_customers
+from superstore;
